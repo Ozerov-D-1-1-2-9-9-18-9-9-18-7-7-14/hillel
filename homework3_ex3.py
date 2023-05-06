f049257bb4be47232b6ed60105d1aeb076d1bb6c
LAPTOP-64N9S1LD\Admin <Danya.ozerov.do@gmail.com>
2023-05-06 23:41:07 +0300

    exercise 3

---
+++ homework3_ex3.py
@@ -0,0 +1,11 @@
+name = input('Enter name: ')
+born = int(input('Enter year of birth: '))
+death = int(input('Enter year of death: '))
+
+age = death - born
+
+if age <= 0:
+    print('Error! Please, enter correct data')
+else:
+    print('Name:', name)
+    print('Age:', age)
