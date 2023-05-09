3aa4c7f8007fc7fb0e9cdc3569a0ab9190d09294
LAPTOP-64N9S1LD\Admin <Danya.ozerov.do@gmail.com>
2023-05-09 11:08:30 +0300

    exercise 1

--- homework3_ex2.py
+++ homework3_ex2.py
@@ -1,1 +1,7 @@
-word = input(f'')
+word = input("Enter 3 word in the format 'snake_case':")
+word1 = word.split('_', maxsplit=-1)
+word2 = word1[0].capitalize() + word1[1].capitalize() + word1[2].capitalize()
+print(word2)
+
+
+
