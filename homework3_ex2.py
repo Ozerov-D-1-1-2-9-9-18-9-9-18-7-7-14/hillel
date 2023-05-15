b994b2289ef22ef9f23464235a58c3b3ff42a825
Ozerov-D-1-1-2-9-9-18-9-9-18-7-7-14 <119451371+Ozerov-D-1-1-2-9-9-18-9-9-18-7-7-14@users.noreply.github.com>
2023-05-15 22:39:09 +0300

    Delete homework3_ex2.py

--- homework3_ex2.py
+++
@@ -1,5 +0,0 @@
-word = input("Enter 3 word in the format 'snake_case':")
-word1 = word.split('_', maxsplit=-1)
-word2 = word1[0].capitalize() + word1[1].capitalize() + word1[2].capitalize()
-print(word2)
-
